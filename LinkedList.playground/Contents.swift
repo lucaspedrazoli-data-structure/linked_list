c4e858f8650b8d6a0876afd0fc72359225ee4846
Lucas <lucaspedrazoli@gmail.com>
2020-08-07 14:08:15 -0300

    create remove last

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -53,3 +53,14 @@ example(of: "pop") {
     print("after popping list: \(list)")
     print("Popped value:  " + String(describing: poppedValue))
 }
+
+example(of: "removing last node") {
+    var list = LinkedList<Int>()
+    list.push(3)
+    list.push(2)
+    list.push(1)
+    print("Before removing last node: \(list)")
+    let removedValue = list.removeLast()
+    print("After removing last node: \(list)")
+    print("Removed value: " + String(describing: removedValue))
+}
