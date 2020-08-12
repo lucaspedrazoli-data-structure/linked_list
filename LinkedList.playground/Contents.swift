565bc8b5a119a43c759740bac7714ff3095e46e3
Lucas <lucaspedrazoli@gmail.com>
2020-08-12 12:49:15 -0300

    test isKnownUniquelyReferenced

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -117,4 +117,9 @@ example(of: "linked list cow") {
     list2.append(3)
     print("list1: \(list1)")
     print("list2: \(list2)")
+
+
+    print("list 1 uniquely referenced: \(isKnownUniquelyReferenced(&list1.head))")
+    let list3 = list1
+    print(print("list 1 uniquely referenced: \(isKnownUniquelyReferenced(&list1.head))"))
 }
